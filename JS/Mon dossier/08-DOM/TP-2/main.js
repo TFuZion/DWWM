05a7a1290ef113c4d4663c26b7df45030566c479
Thomas <thomas02.debray@gmail.com>
2024-01-11 14:26:47 +0100

    TP-2 End

--- JS/Mon dossier/08-DOM/TP-2/main.js
+++ JS/Mon dossier/08-DOM/TP-2/main.js
@@ -1,20 +1,32 @@
-const c = document.getElementById("c"),
-  left = document.getElementById("left"),
-  right = document.getElementById("right"),
-  slash =document.getElementById("slash"),
-  sept = document.getElementById("sept"),
-  huit = document.getElementById("huit"),
-  neuf = document.getElementById("neuf"),
-  star = document.getElementById("star"),
-  cat = document.getElementById("cat"),
-  cinq = document.getElementById("cinq"),
-  six = document.getElementById("six"),
-  moins = document.getElementById("moins"),
-  un = document.getElementById("un"),
-  deux = document.getElementById("deux"),
-  trois = document.getElementById("trois"),
-  plus = document.getElementById("plus"),
-  zero = document.getElementById("zero"),
-  dot = document.getElementById("dot"),
-  egal = document.getElementById("egal")
-  
+const touches = [...document.querySelectorAll(".bouton")]
+const listeKeycode = touches.map(touche => touche.dataset.key)
+const screen = document.querySelector(".screen")
+
+document.addEventListener("click", (e) => {
+  const valeur = e.target.dataset.key;
+  calculer(valeur)
+})
+document.addEventListener("keydown", (e) => {
+  const valeur = e.code.toString();
+  calculer(valeur)
+  console.log(valeur);
+})
+const calculer = (valeur) => {
+  if (listeKeycode.includes(valeur)) {
+
+    switch (valeur) {
+      case 'KeyC':
+        screen.textContent = "";
+        break;
+      case 'Enter':
+        const solus = eval(screen.textContent)
+        screen.textContent = solus;
+        break
+      default:
+        const indexKeycode = listeKeycode.indexOf(valeur);
+        const touche = touches[indexKeycode];
+        screen.textContent += touche.innerHTML;
+    }
+  }
+}
+
